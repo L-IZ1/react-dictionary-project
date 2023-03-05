a167bc865e2ebf17ed072573e96bfc8ef94651aa
L-IZ1 <111218789+L-IZ1@users.noreply.github.com>
2023-03-05 21:20:43 +0100

    added phonetics component

---
+++ src/Phonetics.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Phonetics (props){
+    return(
+        <div className="Phonetics">
+            <a href={props.phonetic.audio} target="_blank">LISTEN</a>{props.phonetic.text}
+        </div>
+    );
+}
--- src/Synonyms.js
+++ src/Synonyms.js
@@ -4,10 +4,10 @@ export default function Synonyms(props){
 
     if (props.synonyms){
         return (
-           <div className="Synonyms"> {props.synonyms.map(function(synonyms,index){
-                return <li key={index}>{synonyms}</li>;
+           <ul className="Synonyms"> {props.synonyms.map(function(synonym,index){
+                return <li key={index}>{synonym}</li>;
             })
-            } </div>);
+            } </ul>);
     }
         else{
         return null;}
