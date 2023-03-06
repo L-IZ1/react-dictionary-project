b5160cac3be73b6ad041fb81cc0d8ad1625beeda
L-IZ1 <111218789+L-IZ1@users.noreply.github.com>
2023-03-06 11:08:32 +0100

    styled app

--- src/App.js
+++ src/App.js
@@ -7,10 +7,10 @@ export default function App() {
     <div className="App">
       <div className="container">
         <header className="App-header">
-          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        <h1>What word are you looking for?</h1>
         </header>
         <main>
-          <Dictionary />
+          <Dictionary defaultkeyword="sustainability"/>
         </main>
         <footer className="App-footer">
           <small>Coded by Elise</small>
--- src/Phonetics.js
+++ src/Phonetics.js
@@ -1,9 +1,11 @@
 import React from "react";
+import "./Phonetics.css";
 
 export default function Phonetics (props){
     return(
         <div className="Phonetics">
+            <span>
             <a href={props.phonetic.audio} target="_blank">LISTEN</a>{props.phonetic.text}
-        </div>
+        </span></div>
     );
 }
