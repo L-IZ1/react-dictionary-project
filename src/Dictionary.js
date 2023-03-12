ed428307d0ff9071694a5807fbc390ed5c568924
L-IZ1 <111218789+L-IZ1@users.noreply.github.com>
2023-03-12 11:06:42 +0100

    added photos component +API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,20 +2,32 @@ import React, { useState } from "react";
 import "./Dictionary.css";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 
 
 export default function Dictionary(props) {
   let [keyword, setKeyword] = useState(props.defaultkeyword);
   let [results, setResults] = useState(null);
   let [loaded,setLoaded] = useState(false);
+  let [photos,setPhotos] =useState(null);
 
 function handleResponse(response) {
     setResults(response.data[0]);
   }
+function handlePexelResponse(response){
+  setPhotos(response.data.photos);
+};
 
 function search(){
 let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
+
+
+   
+   let pexelApiKey="pEHzSWySobVX9zr2OB2w6tKTPv3s92jluvrR7xiXDvJjVYlEN8fLqjem";
+   let pexelApiUrl=`https://api.pexels.com/v1/search?query=${keyword}&per_page=6`; 
+   let headers= {Authorization: `${pexelApiKey}`};
+       axios.get(pexelApiUrl, {headers: headers,}).then(handlePexelResponse);
   }
 
 
@@ -42,6 +54,9 @@ if (loaded){
       </form>
       <div className="hint">Ideas of words to search: climate, ecology, recycle</div></section>
       <Results results={results} />
+      <Photos photos={photos}/>
     </div>
   );}else{load(); return "Loading";}
 }
+
+
